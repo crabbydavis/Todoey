14d6bb845f4967b325b4129eaddf654fa89ca03b
Davis Crabb <davisc@4life.com>
2018-06-30 20:48:57 -0600

    At this point the app is using a PLST to save the data

---
+++ Todoey/Controllers/TodoListViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Davis Crabb on 4/5/18.
+//  Copyright © 2018 Davis Crabb. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("dataFilePath: \(dataFilePath)")
+        
+        let newItem1 = Item()
+        newItem1.title = "Find Mike"
+        let newItem2 = Item()
+        newItem2.title = "Find Bob"
+        let newItem3 = Item()
+        newItem3.title = "Find Fred"
+        
+        itemArray.append(newItem1)
+        itemArray.append(newItem2)
+        itemArray.append(newItem3)
+        
+        loadItems()
+    }
+    
+    // MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    // MARK - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+      
+        tableView.reloadData() // Forces tableView to call resource methods again
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        saveItems()
+    }
+    
+    // MARK - Add new items
+    @IBAction func addItemButton(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            self.saveItems()
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK - Model Manipulation Methods
+    func saveItems() {
+        //self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
+}
+
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListItem.swift
+//  Todoey
+//
+//  Created by Davis Crabb on 4/7/18.
+//  Copyright © 2018 Davis Crabb. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false
+    /*
+    init(itemTitle: String, itemDone: Bool){
+        title = itemTitle
+        done = itemDone
+    }*/
+}
